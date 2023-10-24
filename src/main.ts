bed63b48d9a8785dfdbfdb0694118f89dc14e67a
DimaGrimach <chiki.briki2@gmail.com>
2023-10-24 16:55:39 +0300

    added fixes after second review first task(variable declarate whitespasese in code refomating )

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-!async function bootstrap() {
+async function bootstrap() {
+  const port = process.env.PORT || 3008;
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3008);
-}();
+  await app.listen(port,() => console.log(`Srv started ${port}`));
+}
+bootstrap();
