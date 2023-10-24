e779ac598b1736e8ffaff954b8323ab7526fcf21
DimaGrimach <chiki.briki2@gmail.com>
2023-10-24 16:49:00 +0300

    added fixes after second review

--- src/testik/testik.controller.ts
+++ src/testik/testik.controller.ts
@@ -1,16 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
-import { TestikService } from './testik.service';
+import {Controller, Get} from '@nestjs/common';
+import {TestikService} from './testik.service';
 import {Testik} from "./interface/testik.interface";
 
 @Controller('/')
 export class TestikController {
-  constructor(private readonly testikService: TestikService) {}
+    constructor(private readonly testikService: TestikService) {
+    }
 
-  @Get()
-   async findAll():Promise<Testik> {
-    return await this.testikService.findAll();
-  }
-
+    @Get()
+    async findAll(): Promise<Testik> {
+        return await this.testikService.findAll();
+    }
 
 
 }
