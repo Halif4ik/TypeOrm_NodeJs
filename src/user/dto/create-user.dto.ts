d3cb1c9c436f8e0a223d1fa5c86b156b2f1f128c
DimaGrimach <chiki.briki2@gmail.com>
2023-10-30 21:20:50 +0200

    added migration for new filds users, add post method

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,10 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, Length } from "class-validator";
+export class CreateUserDto {
+
+    @IsEmail({},{message: 'E-mail, should be string'})
+    readonly email:string;
+
+    @IsString({message: 'It should be string'})
+    @Length(4, 10,{ message:'Min lenth 4 max length 10'})
+    readonly password:string;
+}
