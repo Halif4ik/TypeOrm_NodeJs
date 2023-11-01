292cbee58b4407b885eeb95a53c601ffe9e38a67
DimaGrimach <chiki.briki2@gmail.com>
2023-11-01 18:04:32 +0200

    Created DTO objeckts for create/remove/update /get-pagin Users,cleaned up code,renamed folder and interface

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 !async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule,{
+        logger: ['error', 'warn','log'],
+    });
 
     // Define the CORS options
     const corsOptions= {
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,12 +1,12 @@
 import {PartialType} from '@nestjs/mapped-types';
 import {CreateUserDto} from './create-user.dto';
-import {IsEmail, IsOptional, IsString, Length} from "class-validator";
+import {IsEmail, IsString, Length} from "class-validator";
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
-    @IsString()
+    @IsString({message: 'FirstName should be string'})
+    @Length(2, 255,{ message:' firstName Min lenth 2 max length 255'})
     readonly firstName: string;
 
-    @IsString()
-    // @IsOptional()// This decorator allows the property to be optional
+    @IsEmail({},{message: 'E-mail, should be string'})
     readonly email: string;
 }
