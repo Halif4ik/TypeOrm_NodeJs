3574b59383575dc816f6673bc488e6a83da02c91
DimaGrimach <chiki.briki2@gmail.com>
2024-02-19 20:51:54 +0200

    UserService unit tests FINISH

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -9,27 +9,24 @@ import {IUserInfo} from "../GeneralResponse/interface/customResponces";
 import {PassportModule} from "@nestjs/passport";
 import {JwtService} from "@nestjs/jwt";
 import * as bcrypt from "bcryptjs";
+import {HttpStatus} from "@nestjs/common";
+import {UpdateUserDto} from "./dto/update-user.dto";
 
 const mockUserRepositoryMethods = {
    findOneBy: jest.fn(),
+   findOne: jest.fn(),
    create: jest.fn(),
    save: jest.fn(),
    update: jest.fn(),
-   softDelete: jest.fn(),
+   remove: jest.fn(),
    findAndCount: jest.fn(),
-   createQueryBuilder: jest.fn(() => ({
-      leftJoinAndSelect: jest.fn().mockReturnThis(),
-      where: jest.fn().mockReturnThis(),
-      andWhere: jest.fn().mockReturnThis(),
-      orderBy: jest.fn().mockReturnThis(),
-      getManyAndCount: jest.fn(),
-   })),
 };
 
 jest.mock('bcryptjs');
 
 describe('UsersService', () => {
    let userService: UserService;
+   let jwtService: JwtService;
    let userRepository: Repository<User>;
 
    const createUserDto: CreateUserDto = {
@@ -52,7 +49,7 @@ describe('UsersService', () => {
             },
             {
                provide: JwtService, // Provide JwtService
-               useValue: {}, // You can provide a mock object if needed
+               useClass: JwtService,
             },
             {
                provide: getRepositoryToken(User),
@@ -63,69 +60,137 @@ describe('UsersService', () => {
       }).compile();
 
       userService = module.get<UserService>(UserService);
+      jwtService = module.get<JwtService>(JwtService);
       userRepository = module.get<Repository<User>>(getRepositoryToken(User));
-   });
 
+      bcrypt.hash.mockResolvedValue('$2a$05$OhWVMeoV9bojvxmPAaJnT.zdMwhjopG.otOoJX/AnqXUZZgLaza2e');
+   });
 
    describe('createUser', () => {
       it('should create a user', async (): Promise<void> => {
-         bcrypt.hash.mockResolvedValue('$2a$05$OhWVMeoV9bojvxmPAaJnT.zdMwhjopG.otOoJX/AnqXUZZgLaza2e');
 
          jest.spyOn(userRepository, 'findOneBy').mockResolvedValue(null);
          jest.spyOn(userRepository, 'create').mockReturnValue(mockCreatedUser);
          jest.spyOn(userRepository, 'save').mockResolvedValue(mockCreatedUser);
 
-         const result: GeneralResponse<IUserInfo> = await userService.createUser({
-            ...createUserDto,
-            password: '$2a$05$OhWVMeoV9bojvxmPAaJnT.zdMwhjopG.otOoJX/AnqXUZZgLaza2e'
-         });
+         const result: GeneralResponse<IUserInfo> = await userService.createUser(createUserDto);
 
          expect(result.detail.user).toEqual(mockCreatedUser);
          expect(userRepository.findOneBy).toHaveBeenCalledTimes(1);
          expect(userRepository.findOneBy).toHaveBeenCalledWith({email: createUserDto.email});
-         expect(userRepository.create).toHaveBeenCalledWith({...createUserDto,
-            password: '$2a$05$OhWVMeoV9bojvxmPAaJnT.zdMwhjopG.otOoJX/AnqXUZZgLaza2e'});
+         expect(userRepository.create).toHaveBeenCalledWith({
+            ...createUserDto,
+            password: '$2a$05$OhWVMeoV9bojvxmPAaJnT.zdMwhjopG.otOoJX/AnqXUZZgLaza2e'
+         });
       });
+      /**/
+      it('should throw a ConflictException if username already exists', async (): Promise<void> => {
+         jest.spyOn(userRepository, 'findOneBy').mockResolvedValue(mockCreatedUser);
 
+         try {
+            await userService.createUser(createUserDto);
+         } catch (error) {
+            expect(error.response).toEqual('User exist in bd');
+            expect(error.status).toEqual(HttpStatus.CONFLICT);
+         }
+
+         expect(userRepository.findOneBy).toHaveBeenCalledWith({email: createUserDto.email});
+         expect(userRepository.create).not.toHaveBeenCalled();
+      });
    });
 
-});
-
-
-const mockCreatedUser: User = {
-   id: 1,
-   firstName: 'mockName',
-   email: 'mock@test.com',
-   password: '123456',
-   isActive: true,
-   auth: {
-      id: 1,
-      createAt: new Date(),
-      deleteAt: null,
-      upadateAt: new Date(),
-      action_token: 'mockToken',
-      refreshToken: 'mockRefresh token',
-      accessToken: 'mockAccess token',
-      user: null,
-   },
-   roles: [],
-   companyMember: [],
-   invite: [],
-   requests: [],
-   passedQuiz: [
-      /*{
-         id: 10,
-         createDate: new Date(),
-         targetQuiz: {
-            id: 1,
-            frequencyInDay: 7,
-            company: {id: 2, name: 'mocCompanyN', description: 'compDesc', deleteAt: new Date()}
+   describe('updateUserInfo', () => {
+      it('should update user info', async (): Promise<void> => {
+         const updateUserDto: UpdateUserDto = {
+            firstName: 'UpdatedFirstNameMock',
+            email: 'mock@test.com',
          }
-      },*/
-   ],
-   targetForInvite: [],
-   averageRating: [],
-   company: [],
-   notific: [],
-   deleteAt: null,
-};
+         jest.spyOn(jwtService, 'decode').mockReturnValue(mockCreatedUser);
+         jest.spyOn(userRepository, 'findOneBy').mockResolvedValue(mockCreatedUser);
+         jest.spyOn(userRepository, 'save').mockResolvedValue({...mockCreatedUser, ...updateUserDto});
+
+         const result: GeneralResponse<IUserInfo> = await userService.updateUserInfo('tokenFromFront', updateUserDto);
+
+         expect(result.detail.user).toEqual({...mockCreatedUser, ...updateUserDto});
+         expect(userRepository.findOneBy).toHaveBeenCalledTimes(1);
+         expect(userRepository.findOneBy).toHaveBeenCalledWith({email: mockCreatedUser.email});
+         expect(userRepository.save).toHaveBeenCalledTimes(1);
+         expect(userRepository.save).toHaveBeenCalledWith({...mockCreatedUser, ...updateUserDto});
+      });
+      /**/
+      it('should throw a NotFoundException if user not found', async (): Promise<void> => {
+         const updateUserDto: UpdateUserDto = {
+            firstName: 'UpdatedFirstNameMock',
+            email: 'mock@test.com',
+         };
+         jest.spyOn(jwtService, 'decode').mockReturnValue(mockCreatedUser);
+         jest.spyOn(userRepository, 'findOneBy').mockResolvedValue(null);
+
+         try {
+            await userService.updateUserInfo('tokenFromFront', updateUserDto);
+         } catch (error) {
+            expect(error.response).toEqual('User not found');
+            expect(error.status).toEqual(HttpStatus.NOT_FOUND);
+         }
+      });
+
+
+      /**/
+   });
+
+   describe('deleteUser', ():void => {
+      it('should delete user', async (): Promise<void> => {
+         const updateUserDto: UpdateUserDto = {
+            firstName: 'UpdatedFirstNameMock',
+            email: 'mock@test.com',
+         }
+         jest.spyOn(jwtService, 'decode').mockReturnValue(mockCreatedUser);
+         jest.spyOn(userRepository, 'findOne').mockResolvedValue(mockCreatedUser);
+         jest.spyOn(userRepository, 'remove').mockResolvedValue(mockCreatedUser);
+
+         const result: GeneralResponse<IUserInfo> = await userService.deleteUser('tokenFromFront', updateUserDto);
+
+         expect(userRepository.findOne).toHaveBeenCalledTimes(1);
+         expect(result.detail.user).toEqual(mockCreatedUser);
+      });
+   });
+
+   /**/
+   const mockCreatedUser: User = {
+      id: 1,
+      firstName: 'mockName',
+      email: 'mock@test.com',
+      password: '123456',
+      isActive: true,
+      auth: {
+         id: 1,
+         createAt: new Date(),
+         deleteAt: null,
+         upadateAt: new Date(),
+         action_token: 'mockToken',
+         refreshToken: 'mockRefresh token',
+         accessToken: 'mockAccess token',
+         user: null,
+      },
+      roles: [],
+      companyMember: [],
+      invite: [],
+      requests: [],
+      passedQuiz: [
+         /*{
+            id: 10,
+            createDate: new Date(),
+            targetQuiz: {
+               id: 1,
+               frequencyInDay: 7,
+               company: {id: 2, name: 'mocCompanyN', description: 'compDesc', deleteAt: new Date()}
+            }
+         },*/
+      ],
+      targetForInvite: [],
+      averageRating: [],
+      company: [],
+      notific: [],
+      deleteAt: null,
+   };
+});
